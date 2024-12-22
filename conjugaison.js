b7d9b70680772e03ee4b856f04db46d1e7069a0e
Cyrille N'DAH <113162556+23cyy@users.noreply.github.com>
2024-12-22 12:52:47 +0000

    Update conjugaison.js

--- conjugaison.js
+++ conjugaison.js
@@ -1,29 +1,54 @@
-/*Script_conjugaison.html*/
-var premierGroupeT1="<p><b>Présent</b></p><br><p>Je XXXX<b>e</b></p><p>Tu XXXX<b>es</b></p><p>Il/elle XXXX<b>e</b></p><p>Nous XXXX<b>ons</b></p><p>Vous XXXX<b>ez</b></p>Ils/elles XXXX<b>ent</b></p>"; 
-var premierGroupeT2="<p><b>Futur simple</b></p><br><p>Je XXXX<b>erai</b></p><p>Tu XXXX<b>eras</b></p><p>Il/elle XXXX<b>era</b></p><p>Nous XXXX<b>erons</b></p><p>Vous XXXX<b>erez</b></p>Ils/elles XXXX<b>eront</b></p>"; 
-var premierGroupeT3="<p><b>Passé simple</b></p><br><p>Je XXXX<b>ai</b></p><p>Tu XXXX<b>as</b></p><p>Il/elle XXXX<b>a</b></p><p>Nous XXXX<b>âmes</b></p><p>Vous XXXX<b>âtes</b></p>Ils/elles XXXX<b>èrent</b></p>";
+const conjugationTemplates = {
+    1: {
+        Present: "Je XXXX<b>e</b>, Tu XXXX<b>es</b>, Il/elle XXXX<b>e</b>, Nous XXXX<b>ons</b>, Vous XXXX<b>ez</b>, Ils/elles XXXX<b>ent</b>",
+        Futur: "Je XXXX<b>erai</b>, Tu XXXX<b>eras</b>, Il/elle XXXX<b>era</b>, Nous XXXX<b>erons</b>, Vous XXXX<b>erez</b>, Ils/elles XXXX<b>eront</b>",
+        Passe: "Je XXXX<b>ai</b>, Tu XXXX<b>as</b>, Il/elle XXXX<b>a</b>, Nous XXXX<b>âmes</b>, Vous XXXX<b>âtes</b>, Ils/elles XXXX<b>èrent</b>"
+    },
+    2: {
+        Present: "Je XXXX<b>is</b>, Tu XXXX<b>is</b>, Il/elle XXXX<b>it</b>, Nous XXXX<b>issons</b>, Vous XXXX<b>issez</b>, Ils/elles XXXX<b>issent</b>",
+        Futur: "Je XXXX<b>irai</b>, Tu XXXX<b>iras</b>, Il/elle XXXX<b>ira</b>, Nous XXXX<b>irons</b>, Vous XXXX<b>irez</b>, Ils/elles XXXX<b>iront</b>",
+        Passe: "Je XXXX<b>is</b>, Tu XXXX<b>is</b>, Il/elle XXXX<b>it</b>, Nous XXXX<b>îmes</b>, Vous XXXX<b>îtes</b>, Ils/elles XXXX<b>irent</b>"
+    }
+};
 
-function conjuguer()
-{
-    var vrb=document.getElementById('verbc').value; var tps=document.getElementById('tpsc').value
+function conjuguer() {
+    const verb = document.getElementById('verbc').value.toLowerCase();
+    const tense = document.getElementById('tpsc').value;
+    const group = document.getElementById('group').value;
 
-    if (vrb.substr(-2)=="er" && tps=="Present") 
-    {
-        document.getElementById('rp1').innerHTML=premierGroupeT1.replace(/XXXX/g,vrb.substr(0,vrb.length-2));
+    if (!verb || !tense || !group) {
+        alert("Veuillez remplir tous les champs.");
+        return;
     }
-    else if (vrb.substr(-2)=="er" && tps=="Futur simple")
-    {
-        document.getElementById('rp2').innerHTML=premierGroupeT2.replace(/XXXX/g,vrb.substr(0,vrb.length-2));
+
+    const verbRoot = verb.slice(0, -2);
+
+    if ((group == 1 && verb.endsWith('er')) || (group == 2 && verb.endsWith('ir'))) {
+        // Si un seul temps est sélectionné
+        if (tense === "Present") {
+            document.getElementById('rp1').innerHTML = conjugationTemplates[group][tense].replace(/XXXX/g, verbRoot);
+            document.getElementById('rp2').innerHTML = "";
+            document.getElementById('rp3').innerHTML = "";
+        } else if (tense === "Futur") {
+            document.getElementById('rp2').innerHTML = conjugationTemplates[group][tense].replace(/XXXX/g, verbRoot);
+            document.getElementById('rp1').innerHTML = "";
+            document.getElementById('rp3').innerHTML = "";
+        } else if (tense === "Passe") {
+            document.getElementById('rp3').innerHTML = conjugationTemplates[group][tense].replace(/XXXX/g, verbRoot);
+            document.getElementById('rp1').innerHTML = "";
+            document.getElementById('rp2').innerHTML = "";
+        } else if (tense === "Tous les temps") {
+            // Si "Tous les temps" est sélectionné, afficher chaque temps dans son div respectif
+            document.getElementById('rp1').innerHTML = conjugationTemplates[group]["Present"].replace(/XXXX/g, verbRoot);
+            document.getElementById('rp2').innerHTML = conjugationTemplates[group]["Futur"].replace(/XXXX/g, verbRoot);
+            document.getElementById('rp3').innerHTML = conjugationTemplates[group]["Passe"].replace(/XXXX/g, verbRoot);
+        }
+    } else {
+        alert("Verbe non valide pour le groupe sélectionné.");
     }
-    else if (vrb.substr(-2)=="er" && tps=="Passee simple") 
-    {
-        document.getElementById('rp3').innerHTML=premierGroupeT3.replace(/XXXX/g,vrb.substr(0,vrb.length-2));
-    }
-    else if (vrb.substr(-2)=="er" && tps=="Tous les temps") 
-    {
-        document.getElementById('rp1').innerHTML=premierGroupeT1.replace(/XXXX/g,vrb.substr(0,vrb.length-2))
-        document.getElementById('rp2').innerHTML=premierGroupeT2.replace(/XXXX/g,vrb.substr(0,vrb.length-2))  
-        document.getElementById('rp3').innerHTML=premierGroupeT3.replace(/XXXX/g,vrb.substr(0,vrb.length-2));
-    }
-    else alert("Je ne sais pas conjuguer ce verbe\n qui n'appartient pas au 1er groupe !");
+}
+
+
+function generateAllTenses(root, group) {
+    return Object.values(conjugationTemplates[group]).map(template => template.replace(/XXXX/g, root)).join('<br><br>');
 }
